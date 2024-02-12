88dfc3cd2256123d6f80b47abc346c66c08926f4
Dylan Zammit <dylanzam@gmail.com>
2024-02-12 21:46:18 +0100

    use pydantic

--- API/app.py
+++ API/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from football_odds.models import load, DoublePoisson
 from football_odds.utils import ROOT_DIR
 import os
 import uvicorn
+from enum import Enum
 
 
 app = FastAPI()
@@ -10,36 +12,49 @@ app = FastAPI()
 model = load(os.path.join(ROOT_DIR, 'API', 'model.pkl'))
 
 
+class ResultFormats(Enum):
+    odds = 'odds'
+    prob = 'prob'
+
+
+class HomeAwayOdds(BaseModel):
+    home_team: str
+    away_team: str
+    res_fmt: ResultFormats = 'odds'
+
+
 @app.get("/")
 def root():
     return {"message": "Hello, this is a very simple API."}
 
 
-@app.post("/MATCH_ODDS/{home_team}/{away_team}")
-def match_odds(home_team: str, away_team: str, fmt: str = 'odds'):
+@app.post("/match_odds")
+def match_odds(home_away_odds: HomeAwayOdds):
 
     try:
-        res = model.get_match_odds(home_team=home_team, away_team=away_team).match_odds()
+        res = model.get_match_odds(
+            home_team=home_away_odds.home_team,
+            away_team=home_away_odds.away_team
+        ).match_odds()
     except AssertionError:
-        err_msg = f'One of {home_team}, {away_team} does not exist in model'
+        err_msg = f'One of {home_away_odds.home_team}, {home_away_odds.away_team} does not exist in model'
         return {'status': -1, 'message': err_msg}
 
-    if fmt == 'odds':
+    if home_away_odds.res_fmt == ResultFormats.odds:
         return {
             'home': 1 / res[0],
             'draw': 1 / res[1],
             'away': 1 / res[2],
         }
-    elif fmt == 'prob':
+    elif home_away_odds.res_fmt == ResultFormats.prob:
         return {
             'home': res[0],
             'draw': res[1],
             'away': res[2],
         }
+    return {}
 
 
 # Run the server
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-    # EXAMPLE: http://127.0.0.1:8000/MATCH_ODDS/Arsenal/Leicester
